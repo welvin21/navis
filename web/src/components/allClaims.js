dc422d6035eaadbab6857a7201568e0a2990924d
ahsan8244 <syed.ahsan8244@gmail.com>
2019-11-02 02:32:28 +0800

    setup routes

---
+++ web/src/components/allClaims.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const allClaims = () => {
+    return(<div>rendering all claims</div>);
+}
+
+export default allClaims;
---
+++ web/src/components/claimTypes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Layout, Menu, Breadcrumb, Icon } from "antd";
+//import { FirebaseDB } from "../constants/firebase";
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+const test = () => {
+    return(
+        <div>
+            rendering claim types
+        </div>
+    );
+}
+
+export default test;
