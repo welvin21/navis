9a857a03acb76ea1bace85fe3e1296a8ad1ab710
Strelka <strelka@connect.hku.hk>
2019-11-02 09:05:33 +0800

    create color scheme

---
+++ web/src/style/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+    primary: "#0d47a1",
+    primary_light: "#5472d3",
+    primary_dark: "#002171",
+    secondary: "ffeb3b",
+    secondary_light: "ffff72",
+    secondary_dark: "c8b900",
+}
+
+export default colors;
