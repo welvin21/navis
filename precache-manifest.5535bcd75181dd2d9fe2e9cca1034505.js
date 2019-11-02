a8adb77616a90a9faf6973867059f13a9fe59f9d
seanchok <seanchok@gmail.com>
2019-11-02 17:35:25 +0800

    Updates

--- precache-manifest.5535bcd75181dd2d9fe2e9cca1034505.js
+++ precache-manifest.5535bcd75181dd2d9fe2e9cca1034505.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6d3d28a7af502b7e8024bdfd885c42cf",
+    "revision": "15e552e1521c7fa67bf608030ef360c7",
     "url": "/index.html"
   },
   {
