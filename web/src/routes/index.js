f3a957930d16b5e9381e382bb85d30bdde661268
Diab haque <diab@connect.hku.hk>
2019-11-02 16:00:05 +0800

    Web: Added Seller form

--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import AppBar from "../components/AppBar";
 import AllTransactions from "../components/AllTransactions";
 import MakeClaim from "../components/MakeClaim";
+import SellerForm from "../forms/SellerForm.js";
 
 class Index extends React.Component {
   render() {
@@ -12,6 +13,7 @@ class Index extends React.Component {
           <AppBar>
             <Route exact path="/" component={AllTransactions} />
             <Route exact path="/makeClaim" component={MakeClaim} />
+            <Route exact path="/openSellerForm" component={SellerForm}/>
           </AppBar>
         </Switch>
       </BrowserRouter>
