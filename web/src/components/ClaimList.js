b79dde01fee395d978897a3e727410f6e0b2eec5
seanchok <seanchok@gmail.com>
2019-11-02 08:25:43 +0800

    create ClaimList and prompt firebaseDB

--- web/src/components/ClaimList.js
+++ web/src/components/ClaimList.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {FirebaseDB as db} from '../constants/firebase';
 
 const ClaimList = () => {
+  const [claims, setClaims] = useState([]);
+  useEffect(() => {
+    db.collection('claims').get().then(querySnapshot => {
+      let i = 0;
+      querySnapshot.forEach((doc) => {
+        setClaims(doc.data().claims);
+      })
+    })
+  })
   return (
-    <div>
-      list
+    <div style={{margin: '20px auto'}}>
+      {
+        
+      }
     </div>
   )
 }
