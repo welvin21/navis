5520d8d7224c6601267835d3ad56d0b613416147
Welvin <wewe2712@connect.hku.hk>
2019-11-02 10:43:15 +0800

    transactions : change filenames

--- web/src/components/TransactionCard.js
+++ web/src/components/TransactionCard.js
@@ -1,81 +1,57 @@
-import React from "react";
-import { Card, Timeline, Row, Col, Tag, Typography } from "antd";
-
-const { Title, h1, h2, h3, h4 } = Typography;
+import React from 'react';
+import { Card, Timeline, Row, Col, Tag } from "antd";
 
 const style = {
-  container: {
-    display: "grid",
-    gridTemplateColumns: "70% 30%"
-  },
-  card: {
-    borderRadius: 10
-  }
-};
+    container : {
+        display : 'grid',
+        gridTemplateColumns : '70% 30%',
+    },
+    card : {
+        borderRadius : 10
+    }
+}
 
-const TransactionCard = props => {
-  const { data, onClickFunction } = props;
-  return (
-    <Row>
-      <Col span={3} style={{ textAlign: "justify", marginTop: "8px" }}>
-        <Title level={3} style={{ lineHeight: "1" }}>
-          <b>20</b>
-          <br></br>
-          AUG
-        </Title>
-      </Col>
-      <Col span={21}>
+const TransactionCard = (props) => {
+    const { data } = props;
+    return (
         <Card
-          title={`#Transaction ${data.id}`}
-          size="default"
-          hoverable
-          style={style.card}
-          onClick={() => {
-            onClickFunction(data);
-          }}
+        title={`#Claim ${data.id}`}
+        size="default"
+        hoverable
+        style={style.card}
         >
-          <div style={style.container}>
-            <Timeline>
-              <Timeline.Item>{data.placeOfExport}</Timeline.Item>
-              <Timeline.Item>{data.placeOfImport}</Timeline.Item>
-            </Timeline>
-            <Tag color="magenta" style={{ height: 20, width: "auto" }}>
-              {data.goods}
-            </Tag>
-          </div>
-          <Row>
-            <Col span={8}>
-              <b>Seller</b>
-            </Col>
-            <Col span={16}>{data.sellerName}</Col>
-          </Row>
-          <Row>
-            <Col span={8}>
-              <b>Buyer</b>
-            </Col>
-            <Col span={16}>{data.buyerName}</Col>
-          </Row>
-          <Row>
-            <Col span={8}>
-              <b>Contract No.</b>
-            </Col>
-            <Col span={16}>{data.contractNumber}</Col>
-          </Row>
-          <Row>
-            <Col span={8}>
-              <b>Document No.</b>
-            </Col>
-            <Col span={16}>{data.documentNumber}</Col>
-          </Row>
-          <Row>
-            <Col span={8}>
-              <b>Invoice No.</b>
-            </Col>
-            <Col span={16}>{data.invoiceNumber}</Col>
-          </Row>
+            <div style={style.container}>
+                <Timeline>
+                    <Timeline.Item>{data.placeOfExport}</Timeline.Item>
+                    <Timeline.Item>{data.placeOfImport}</Timeline.Item>
+                </Timeline>
+                <Tag color='magenta' style={{height : 20,width : 'auto'}}>
+                    {data.goods}
+                </Tag>
+            </div>
+            <Row>
+                <Col span={8}><b>Seller</b></Col>
+                <Col span={16}>{data.sellerName}</Col>
+            </Row>
+            <Row>
+                <Col span={8}><b>Buyer</b></Col>
+                <Col span={16}>{data.buyerName}</Col>
+            </Row>    
+            <Row>
+                <Col span={8}><b>Contract No.</b></Col>
+                <Col span={16}>{data.contractNumber}</Col>
+            </Row>    
+            <Row>
+                <Col span={8}><b>Document No.</b></Col>
+                <Col span={16}>{data.documentNumber}</Col>
+            </Row>    
+            <Row>
+                <Col span={8}><b>Invoice No.</b></Col>
+                <Col span={16}>{data.invoiceNumber}</Col>
+            </Row>      
         </Card>
-      </Col>
-    </Row>
-  );
-};
+    );
+
+}
+
 export default TransactionCard;
--- web/src/components/TransactionsList.js
+++ web/src/components/TransactionsList.js
@@ -1,32 +1,27 @@
-import React, { useEffect, useState } from "react";
-import TransactionCard from "./TransactionCard";
-import { FirebaseDB as db } from "../constants/firebase";
+import React, {useEffect, useState} from 'react';
+import TransactionCard from './TransactionCard';
+import {FirebaseDB as db} from '../constants/firebase';
 
-const TransactionsList = props => {
-  const { onClickFunction } = props;
+const TransactionsList = () => {
   const [claims, setClaims] = useState([]);
   useEffect(() => {
-    db.collection("claims")
-      .get()
-      .then(querySnapshot => {
-        querySnapshot.forEach(doc => {
-          setClaims(doc.data().claims);
-        });
-      });
-  });
+    db.collection('claims').get().then(querySnapshot => {
+      querySnapshot.forEach((doc) => {
+        setClaims(doc.data().claims);
+      })
+    })
+  })
   return (
-    <div style={{ margin: "20px auto" }}>
-      {claims.map(claim => {
-        return (
-          <TransactionCard
-            key={claim.id}
-            data={claim}
-            onClickFunction={onClickFunction}
-          />
-        );
-      })}
+    <div style={{margin: '20px auto'}}>
+      {
+        claims.map(claim => {
+          return(
+            <TransactionCard data={claim}/>
+          );
+        })
+      }
     </div>
-  );
-};
+  )
+}
 
 export default TransactionsList;
