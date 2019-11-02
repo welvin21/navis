22586e2ab8de812944a5eb1c3b2f8a3d87a6d488
uday1331 <u3555276@connect.hku.hk>
2019-11-02 20:29:48 +0800

    web: refactored for Seller Dashboard

--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import React from "react";
 import AppBar from "../components/AppBar";
+import SellerBar from "../components/SellerBar";
 import AllTransactions from "../components/AllTransactions";
 import MakeClaim from "../components/MakeClaim";
 import Notifications from "../components/Notifications";
@@ -8,19 +9,36 @@ import Notifications from "../components/Notifications";
 import SellerForm from "../forms/SellerForm.js";
 
 class Index extends React.Component {
-  render() {
+  AppBarLayout = props => {
     return (
-      <BrowserRouter>
+      <Switch>
+        <AppBar>
+          <Route exact path="/" component={AllTransactions} />
+          <Route exact path="/allTransactions" component={AllTransactions} />
+          <Route exact path="/makeClaim" component={MakeClaim} />
+          <Route exact path="/notifications" component={Notifications} />
+        </AppBar>
+      </Switch>
+    );
+  };
+
+  SellerLayout = () => {
+    return (
+      <SellerBar>
         <Switch>
-          <AppBar>
-            <Route exact path="/" component={AllTransactions} />
-            <Route exact path="/allTransactions" component={AllTransactions} />
-            <Route exact path="/makeClaim" component={MakeClaim} />
-            <Route exact path="/notifications" component={Notifications} />
-            <Route exact path="/openSellerForm" component={SellerForm}/>
-          </AppBar>
+          <Route exact path="/seller/openSellerForm" component={SellerForm} />
         </Switch>
-      </BrowserRouter>
+      </SellerBar>
+    );
+  };
+
+  render() {
+    const { AppBarLayout, SellerLayout } = this;
+    return (
+      <Switch>
+        <Route path="/seller" component={SellerLayout} />
+        <Route path="/" component={AppBarLayout} />
+      </Switch>
     );
   }
 }
