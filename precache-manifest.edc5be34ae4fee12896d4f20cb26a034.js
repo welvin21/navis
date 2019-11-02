76a178c4b132fc370e2e6d59ce96bcb6917fe5e7
seanchok <seanchok@gmail.com>
2019-11-03 06:11:29 +0800

    Updates

---
+++ precache-manifest.edc5be34ae4fee12896d4f20cb26a034.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "650c28c4127a266ff32d9c36188584d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9baeeeffb0be32cf773",
+    "url": "/static/css/2.65d0fb34.chunk.css"
+  },
+  {
+    "revision": "2ecf1d27210dd3e4316a",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "b9baeeeffb0be32cf773",
+    "url": "/static/js/2.139beb5f.chunk.js"
+  },
+  {
+    "revision": "2ecf1d27210dd3e4316a",
+    "url": "/static/js/main.bb36547e.chunk.js"
+  },
+  {
+    "revision": "500c6a4d9d125d185445",
+    "url": "/static/js/runtime-main.4ea2b256.js"
+  },
+  {
+    "revision": "22efcc593bc792c7bd5eca7b8c61cb02",
+    "url": "/static/media/Navis_deconstructed_transparent.22efcc59.png"
+  },
+  {
+    "revision": "499e135f63720c43ffdb0fdaa44a24f7",
+    "url": "/static/media/Navis_icon_transparent.499e135f.png"
+  },
+  {
+    "revision": "1d6fb25cc0078068959c81ec264e46cb",
+    "url": "/static/media/navis_logo.1d6fb25c.png"
+  }
+]);
