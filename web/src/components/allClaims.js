3593fceb70fa40772f16fc629d0457a3f9af35dc
Sean Chok <seanchok@gmail.com>
2019-11-02 10:44:22 +0800

    resolve router issues (#7)

--- web/src/components/allClaims.js
+++ web/src/components/allClaims.js
@@ -1,36 +1,55 @@
-import React, { useState } from 'react';
-import {Typography, Menu, Dropdown, Icon} from 'antd';
-import ClaimList from './ClaimList';
+import React, { useState } from "react";
+import { Layout, Typography, Menu, Dropdown, Icon } from "antd";
+import ClaimList from "./ClaimList";
 
-const {Title, Text} = Typography;
+const { Content, Sider } = Layout;
+const { Title, Text } = Typography;
 
 const AllClaims = () => {
-    const list = ['latest', 'earliest', 'claimant name'];
-    const [sortCriteria, setSortCriteria] = useState('latest');
+  const list = ["latest", "earliest", "claimant name"];
+  const [sortCriteria, setSortCriteria] = useState("latest");
 
-    const menu = (
-        <Menu onClick={(e) => {setSortCriteria(e.key)}}>
-            {
-                list.map((item) => (
-                    <Menu.Item key={item}>
-                        <Text>{item}</Text>
-                    </Menu.Item>
-                ))
-            }
-        </Menu>
-    )
-    return (
-        <div style={{margin: 'auto 20px'}}>
+  const menu = (
+    <Menu
+      onClick={e => {
+        setSortCriteria(e.key);
+      }}
+    >
+      {list.map(item => (
+        <Menu.Item key={item}>
+          <Text>{item}</Text>
+        </Menu.Item>
+      ))}
+    </Menu>
+  );
+  return (
+    <Layout style={{ padding: "24px 0", background: "#fff" }}>
+      <Sider width={400} style={{ background: "#fff" }}>
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={["1"]}
+          defaultOpenKeys={["sub1"]}
+          style={{ height: "100%" }}
+        >
+          <div style={{ margin: "auto 20px" }}>
             <Title>All Claims</Title>
             <Text>sorted by </Text>
             <Dropdown overlay={menu}>
-                <Text style={{color: 'blue'}}>
-                    {sortCriteria} {list[sortCriteria]} <Icon type='down' />
-                </Text>
+              <Text style={{ color: "blue" }}>
+                {sortCriteria} {list[sortCriteria]} <Icon type="down" />
+              </Text>
             </Dropdown>
-            <ClaimList sortCriteria={sortCriteria}/>
-        </div>
-    );
-}
+            <ClaimList sortCriteria={sortCriteria} />
+          </div>
+        </Menu>
+      </Sider>
+      <Content style={{ padding: "0 24px", height: "540px" }}>
+        {
+          // right content
+        }
+      </Content>
+    </Layout>
+  );
+};
 
 export default AllClaims;
