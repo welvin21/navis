42e1001613a5cdbab4f48900e7abefd5f42ec7b9
seanchok <seanchok@gmail.com>
2019-11-02 10:43:47 +0800

    resolve router issues

--- web/src/components/AppBar.js
+++ web/src/components/AppBar.js
@@ -2,7 +2,7 @@ import React from "react";
 import "antd/dist/antd.css";
 import { Layout, Menu, Icon } from "antd";
 //import { FirebaseDB } from "../constants/firebase";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const { Header, Content, Sider } = Layout;
 
@@ -25,42 +25,25 @@ class AppBar extends React.Component {
         >
           <div className="logo" />
           <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
-              <Menu.Item key="1">
-                <Icon type="pie-chart" />
-                <span>All Claims</span>
-                <Link to ="/"></Link>
-              </Menu.Item>
+            <Menu.Item key="1">
+              <Icon type="pie-chart" />
+              <span>All Claims</span>
+              <Link to="/"></Link>
+            </Menu.Item>
             <Menu.Item key="2">
               <Icon type="desktop" />
               <span>Make Claim</span>
-              <Link to ="/claimTypes"></Link>
+              <Link to="/claimTypes"></Link>
             </Menu.Item>
-
           </Menu>
         </Sider>
         {
-          //conten on the right
+          //content on the right
         }
         <Layout>
           <Header style={{ background: "#fff", padding: 0 }} />
           <Content style={{ padding: "0 20px", marginTop: "20px" }}>
-            <Layout style={{ padding: "24px 0", background: "#fff" }}>
-              <Sider width={400} style={{ background: "#fff" }}>
-                <Menu
-                  mode="inline"
-                  defaultSelectedKeys={["1"]}
-                  defaultOpenKeys={["sub1"]}
-                  style={{ height: "100%" }}
-                >
-                  {
-                    this.props.children
-                  }
-                </Menu>
-              </Sider>
-              <Content style={{ padding: "0 24px", height: "540px" }}>
-                
-              </Content>
-            </Layout>
+            {this.props.children}
           </Content>
         </Layout>
       </Layout>
